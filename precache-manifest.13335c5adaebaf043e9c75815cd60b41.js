b990f71dae33d5de873c85c0baf7cf9a7d10829e
Dave <davebaxtermusic@gmail.com>
2019-11-27 23:22:32 +1300

    Updates

---
+++ precache-manifest.13335c5adaebaf043e9c75815cd60b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9654cf9efe7c2c62b5fdd708bb9996a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6772662f10c16161c8e2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b35ede367a2a175b1e93",
+    "url": "/static/js/2.fa249320.chunk.js"
+  },
+  {
+    "revision": "6772662f10c16161c8e2",
+    "url": "/static/js/main.886d2718.chunk.js"
+  },
+  {
+    "revision": "41b5f16d93e59e253851",
+    "url": "/static/js/runtime-main.d91f80d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.13335c5adaebaf043e9c75815cd60b41.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
